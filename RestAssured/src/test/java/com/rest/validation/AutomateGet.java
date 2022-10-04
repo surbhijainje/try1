a4a5ee4321518025ef2032ad12b56fa45d4ef1dc
Harshil <Harshil@DESKTOP-4H7SDUP>
2022-10-04 06:58:39 +0530

    Updating AutomateGet

--- RestAssured/src/test/java/com/rest/validation/AutomateGet.java
+++ RestAssured/src/test/java/com/rest/validation/AutomateGet.java
@@ -6,14 +6,22 @@ import static org.hamcrest.Matchers.*;
 
 import org.testng.annotations.Test;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
 public class AutomateGet {
 
 	@Test(enabled = false)
 	public void validate_get_status_code() {
 
 		given().baseUri("https://api.getpostman.com")
-				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").when()
-				.get("/workspaces").then().log().all().assertThat().statusCode(201);
+				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
+				when()
+				   .get("/workspaces").
+				          then().
+				               log().all().
+				                    assertThat().
+				                         statusCode(201);
 
 	}
 
@@ -35,9 +43,58 @@ public class AutomateGet {
 						"workspaces.size()",equalTo(3));
 	      }
 	
-	   
+	 @Test
+	  public void extract_response() {
+		
+		 Response res = given().
+		       baseUri("https://api.getpostman.com")
+			   .header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
+        when()
+			 .get("/workspaces").
+	     then().
+	          log().all().
+	            assertThat().
+	              statusCode(200).extract().response();
+		 
+		 System.out.println("Extracted Response"    +res.asString());
+	  
+	  }
+	 
+	 @Test
+	  public void extract_single_response() {
+		
+		 Response res = given().
+		       baseUri("https://api.getpostman.com")
+			   .header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
+       when()
+			 .get("/workspaces").
+	     then().
+	          log().all().
+	            assertThat().
+	              statusCode(200).extract().response();
+		 
+		 System.out.println("Extracted Response"    +res.path("workspaces[0].name"));
+	  
+	  }
+	 
+	 
+	 @Test
+	  public void extract_single_response2() {
+		
+		 Response res = given().
+		       baseUri("https://api.getpostman.com")
+			   .header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
+      when()
+			 .get("/workspaces").
+	     then().
+	          log().all().
+	            assertThat().
+	              statusCode(200).
+	              extract().response();
+		 
+		 JsonPath jsonpath = new JsonPath(res.asString());
+		 
+		 System.out.println("Extracting single value :"  +jsonpath.getString("workspaces[1].name"));
 	
-	
-	
-
+	   }
 }
