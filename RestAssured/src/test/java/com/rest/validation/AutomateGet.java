e27bfaf5945fcc9772573b8ecfddb8f7204dcb7e
Harshil <Harshil@DESKTOP-4H7SDUP>
2022-10-06 07:05:42 +0530

    Adding Hamcrest Validation on responses

--- RestAssured/src/test/java/com/rest/validation/AutomateGet.java
+++ RestAssured/src/test/java/com/rest/validation/AutomateGet.java
@@ -2,8 +2,11 @@ package com.rest.validation;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
 
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.path.json.JsonPath;
@@ -15,86 +18,96 @@ public class AutomateGet {
 	public void validate_get_status_code() {
 
 		given().baseUri("https://api.getpostman.com")
-				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
-				when()
-				   .get("/workspaces").
-				          then().
-				               log().all().
-				                    assertThat().
-				                         statusCode(201);
+				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").when()
+				.get("/workspaces").then().log().all().assertThat().statusCode(201);
 
 	}
 
-	@Test
+	@Test(enabled = false)
 	public void validate_response_body() {
 
 		given().baseUri("https://api.getpostman.com")
-				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
-	       when()
-				.get("/workspaces").
-		    then().log().all().
-		           assertThat().
-		              statusCode(200).
-		              body("workspaces.name",
+				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").when()
+				.get("/workspaces").then().log().all().assertThat().statusCode(200).body("workspaces.name",
 						hasItems("APITESTING", "APIBATCH_9PM", "My Workspace"),
 
-						"workspaces.type", hasItems("personal", "personal", "personal"),
-						"workspaces[0].name",  equalTo("APITESTING"),
-						"workspaces.size()",equalTo(3));
-	      }
-	
-	 @Test
-	  public void extract_response() {
+						"workspaces.type", hasItems("personal", "personal", "personal"), "workspaces[0].name",
+						equalTo("APITESTING"), "workspaces.size()", equalTo(3));
+	}
+
+	@Test(enabled = false)
+	public void extract_response() {
+
+		Response res = given().baseUri("https://api.getpostman.com")
+				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").when()
+				.get("/workspaces").then().log().all().assertThat().statusCode(200).extract().response();
+
+		System.out.println("Extracted Response" + res.asString());
+
+	}
+
+	@Test(enabled = false)
+	public void extract_single_response() {
+
+		Response res = given().baseUri("https://api.getpostman.com")
+				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").when()
+				.get("/workspaces").then().log().all().assertThat().statusCode(200).extract().response();
+
+		System.out.println("Extracted Response" + res.path("workspaces[0].name"));
+
+	}
+
+	@Test(enabled = false)
+	public void extract_single_response2() {
+
+		Response res = given().baseUri("https://api.getpostman.com")
+				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").when()
+				.get("/workspaces").then().log().all().assertThat().statusCode(200).extract().response();
+
+		JsonPath jsonpath = new JsonPath(res.asString());
+
+		System.out.println("Extracting single value :" + jsonpath.getString("workspaces[1].name"));
+
+	}
+
+	@Test(enabled = true)
+	public void hamcrest_assert_on_extracted_response() {
+
+		String name = given().baseUri("https://api.getpostman.com")
+				.header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").when()
+				.get("/workspaces").then().
+				// log().all().
+				assertThat().statusCode(200).extract().response().path("workspaces[0].name");
+
+		System.out.println("Workspace name =" + name);
+
+		assertThat(name, equalTo("APITESTING"));
+
+		// Assert.assertEquals(name, "APITESTING2");
+
+	}
+
+	@Test(enabled = true)
+	  public void hamcrest_assert_on_extracted_response1() {
 		
-		 Response res = given().
+		  given().
 		       baseUri("https://api.getpostman.com")
 			   .header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
-        when()
+    when()
 			 .get("/workspaces").
 	     then().
-	          log().all().
-	            assertThat().
-	              statusCode(200).extract().response();
-		 
-		 System.out.println("Extracted Response"    +res.asString());
-	  
-	  }
-	 
-	 @Test
-	  public void extract_single_response() {
-		
-		 Response res = given().
-		       baseUri("https://api.getpostman.com")
-			   .header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
-       when()
-			 .get("/workspaces").
-	     then().
-	          log().all().
-	            assertThat().
-	              statusCode(200).extract().response();
-		 
-		 System.out.println("Extracted Response"    +res.path("workspaces[0].name"));
-	  
-	  }
-	 
-	 
-	 @Test
-	  public void extract_single_response2() {
-		
-		 Response res = given().
-		       baseUri("https://api.getpostman.com")
-			   .header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d").
-      when()
-			 .get("/workspaces").
-	     then().
-	          log().all().
+	          //log().all().
 	            assertThat().
 	              statusCode(200).
-	              extract().response();
-		 
-		 JsonPath jsonpath = new JsonPath(res.asString());
-		 
-		 System.out.println("Extracting single value :"  +jsonpath.getString("workspaces[1].name"));
-	
-	   }
+	              body("workspaces.name", contains("APITESTING","APIBATCH_9PM"),
+	            		"workspaces.type", hasItems("personal","personal"),
+	            		"workspaces[0].name", equalTo("APITESTING"),
+	            		"workspaces.size()" ,equalTo(3)
+	            		         
+	            		  
+	       );
+	             
+	}	 
+	 
+		
 }
