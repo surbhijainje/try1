d3444f702040e5998e4ebf2e77018c70f649a459
Harshil <Harshil@DESKTOP-4H7SDUP>
2022-10-12 07:09:10 +0530

    Adding ResponseSpecification snippet

---
+++ RestAssured/src/test/java/com/rest/validation/AutomationPost.java
@@ -0,0 +1,6 @@
+package com.rest.validation;
+
+public class AutomationPost {
+	
+	
+}
--- RestAssured/src/test/java/com/rest/validation/RequestSpecification1.java
+++ RestAssured/src/test/java/com/rest/validation/RequestSpecification1.java
@@ -8,65 +8,74 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.*;
 
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
 public class RequestSpecification1 {
-	
-	//RequestSpecification requestSpecification;
-	
-    //public static RequestSpecification xyz;
-	
-	//DefaultRequestSpecification
+
+	// ResponseSpecification responseSpecification;
+
+	// RequestSpecification requestSpecification;
+
+	// public static RequestSpecification xyz;
+
+	// DefaultRequestSpecification
 	@BeforeClass
 	public void beforeClass() {
-		
-	
-		
-	/* requestSpecification= with().
-				baseUri("https://api.getpostman.com").
-		        header("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d"); */
-		
-		RequestSpecBuilder requestSpecBuilder=new RequestSpecBuilder();
-	    
+
+		/*
+		 * requestSpecification= with(). baseUri("https://api.getpostman.com").
+		 * header("X-Api-Key",
+		 * "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d");
+		 */
+
+		RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+
 		requestSpecBuilder.setBaseUri("https://api.getpostman.com");
 		requestSpecBuilder.addHeader("X-Api-Key", "PMAK-6334a0323ce9283aae75733b-c31406c4a94e35ab5d0cce1793d1678e7d");
 		requestSpecBuilder.log(LogDetail.ALL);
-		RestAssured.requestSpecification=requestSpecBuilder.build();
-		
-		
-		
-	            
+		RestAssured.requestSpecification = requestSpecBuilder.build();
+
+		/*
+		 * responseSpecification= RestAssured.expect(). statusCode(200).
+		 * contentType(ContentType.JSON). log().all();
+		 */
+
+		ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder().expectStatusCode(200)
+				.expectContentType(ContentType.JSON).log(LogDetail.ALL);
+
+		RestAssured.responseSpecification = responseSpecBuilder.build();
+
 	}
-	
+
 	@Test
 	public void validate_statuscode() {
+
 		
-		Response response =get("/workspaces").then(). log().all().extract().response();
-	  assertThat(response.statusCode(),is(equalTo(200)));      
+		
+		get("/workspaces");
 
 	}
-	
-	
-	@Test(enabled = false)
+
+	@Test(enabled = true)
 	public void validate_response_body() {
-		
-		
-		Response response = get("/workspaces").then().log().all().extract().response();
-		 assertThat(response.statusCode(),is(equalTo(200)));
-		 assertThat(response.path("workspaces.name"), equalTo("APITESTING"));
-		
-		
-	}
 
-	
-	
-	
+		Response response = get("/workspaces").then().extract().response();
+
+		/*
+		 * assertThat(response.body("workspaces.name", hasItems("APITESTING",
+		 * "APIBATCH_9PM", "My Workspace")) );
+		 */
+
+	}
 
 }
